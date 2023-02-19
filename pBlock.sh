a175bd01c4a01608db42d7b4599fd2963777a409
k3rnelpanik <36093328+k3rnelpanik@users.noreply.github.com>
2023-02-19 11:56:38 +0530

    Cleaned out adblocking hosts
    Removed adblocking hosts, added temporary directory to store hosts from multiple sources before combining them.

--- pBlock.sh
+++ pBlock.sh
@@ -1,5 +1,19 @@
-echo ">>> Downloading StevenBlack Porn Hosts"
-wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn/hosts -O /data/adb/modules/hosts/system/etc/hosts
+echo ">>> Checking for temporary directory"
+echo ">>> Downloading Hosts To Block"
+if [ ! -d /sdcard/pblock ]; then
+  mkdir /sdcard/pblock
+fi
+wget https://raw.githubusercontent.com/StevenBlack/hosts/master/extensions/porn/bigdargon/hosts -O /sdcard/pblock/hosts
+wget https://raw.githubusercontent.com/StevenBlack/hosts/master/extensions/porn/brijrajparmar27/hosts -O /sdcard/pblock/bri
+wget https://raw.githubusercontent.com/StevenBlack/hosts/master/extensions/porn/clefspeare13/hosts -O /sdcard/pblock/clef
+wget https://raw.githubusercontent.com/StevenBlack/hosts/master/extensions/porn/sinfonietta-snuff/hosts -O /sdcard/pblock/sinsnu
+wget https://raw.githubusercontent.com/StevenBlack/hosts/master/extensions/porn/sinfonietta/hosts -O /sdcard/pblock/sin
+wget https://raw.githubusercontent.com/StevenBlack/hosts/master/extensions/porn/tiuxo/hosts -O /sdcard/pblock/tiu
+for file in /sdcard/pblock/bri /sdcard/pblock/clef /sdcard/pblock/sinsnu /sdcard/pblock/sin /sdcard/pblock/tiu
+do
+  cat "$file" >> /sdcard/pblock/hosts && rm "$file"
+done
+cp /sdcard/pblock/hosts /data/adb/modules/hosts/system/etc/hosts
 cp /data/adb/modules/hosts/system/etc/hosts /data/adb/modules/hosts/system/etc/hostse
 echo ">>> Done"
 echo ">>> Script by k3ernelpanik"
